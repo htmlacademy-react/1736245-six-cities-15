5e5de8489920da1f49688aa596d28a535b1203cb
reachoutfaith <anastasiya@MacBook-Pro-Romenskaya.local>
2024-02-28 19:07:00 +0300

    добавила руты, страницу not found

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,14 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 import Main from '../../pages/main/main';
-// import Login from '../../pages/login/login';
-// import Offer from '../../pages/offer/offer';
-// import Favorites from '../../pages/favorites/favorites';
+import Login from '../../pages/login/login';
+import Offer from '../../pages/offer/offer';
+import Favorites from '../../pages/favorites/favorites';
+import PageNotFound from '../../pages/page-not-found/page-not-found';
 
 type TPlacesToStay = {
   placesToStay: number;
@@ -9,12 +16,30 @@ type TPlacesToStay = {
 
 function App({placesToStay}: TPlacesToStay) {
   return (
-    <>
-      <Main placesToStay = {placesToStay}/>
-      {/* <Login/>
-      <Offer/>
-      <Favorites/> */}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Main placesToStay = {placesToStay} />}
+        />
+        <Route
+          path='/login'
+          element={<Login />}
+        />
+        <Route
+          path='/offer/:id'
+          element={<Offer />}
+        />
+        <Route
+          path='/favorites'
+          element={<Favorites />}
+        />
+        <Route
+          path='*'
+          element={<PageNotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/page-not-found/page-not-found.tsx
@@ -0,0 +1,12 @@
+import Header from '../../components/header/header';
+
+const PageNotFound = () => (
+  <div className="page">
+    <Header/>
+    <main className="page__main page__main--offer">
+      <h3>Oops! Error 404! Page is not found. </h3>
+    </main>
+  </div>
+);
+
+export default PageNotFound;
